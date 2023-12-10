41772396d8e106e10f91332bfa823367158a1c85
Anton Lundgren <anton.lundgren94@gmail.com>
2023-12-10 17:50:44 +0100

    day 5 complete

--- day5.py
+++ day5.py
@@ -8,15 +8,28 @@ def get_destination(source, convert_map):
     return destination
 
 def get_destinations(source, s_range, convert_map):
-    destinations = [[source, s_range]]
+    destinations = []
     for x in convert_map:
-        if x[1] <= source < x[1] + x[2]:
-            r = [x[0] + (source - x[1]), min(s_range - (x[1] - source), x[2] - (source - x[1]))]
-            destinations.append(r)
-            if r[1] != s_range - (x[1] - source):
-                destinations.extend(get_destinations(source + r[1], s_range - r[1], convert_map))
-            break
-    # print(destinations)
+        # Check if overlap
+        if not (source + s_range < x[1] or source > x[1] + x[2]):
+            overlap_start = max(source, x[1])
+            overlap_end = min(source + s_range, x[1] + x[2])
+            # Convert
+            destinations.append([overlap_start, overlap_end - overlap_start, x[0] + overlap_start - x[1]])
+    destinations.sort()
+    # Insert the unlisted mappings
+    point = source
+    missing = []
+    for x in destinations:
+        if x[0] > point:
+            missing.append([point, x[0] - point, point])
+        point = x[0] + x[1]
+    if point < source + s_range:
+        missing.append([point, source + s_range - point, point])
+    destinations.extend(missing)
+    # Reformat
+    for i in range(len(destinations)):
+        destinations[i] = [destinations[i][2], destinations[i][1]]
     return destinations
 
 with open("day5input.txt") as f:
@@ -62,11 +75,9 @@ for i in range(0, len(seeds), 2):
         for d in destinations:
             new_destinations.extend(get_destinations(d[0], d[1], m))
         destinations = new_destinations
-        print(destinations)
     seed_to_location[seeds[i]] = destinations
    
 answer = float("inf")
-# print(seed_to_location)
 for value in seed_to_location.values():
     for v in value:
         if v[0] < answer:
